98fdebed4a376b5579e9470f7f2020d0750b40e2
misha <mihailshagoiko@gmail.com>
2016-08-31 21:11:09 +0300

    Header

--- js/common.js
+++ js/common.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+	function wResize() {
+		$("header")	.css("min-height", $(window).height());
+	}
+	wResize();
+	$(window).resize(function () {
+		wResize();
+	});
+
+	$(".top_phone .wrapper .tab").click(function() {
+		$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$(".tabs_header .wrapper .tab").click(function() {
+		$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
